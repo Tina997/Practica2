f34e9df28281dec9d60ea534ebacdd11fc916589
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2020-10-20 23:40:08 +0100

    Implementacion base de datos

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 //Indicar la ruta a la que llama la variable
 var serviciosRouter = require('./routes/servicios');
+var dbRouter = require('./routes/db');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 //Indicar que /servicios va a la ruta indicada 
 app.use('/servicios', serviciosRouter);
+app.use('/db', dbRouter);
 
 
 // catch 404 and forward to error handler
--- routes/servicios.js
+++ routes/servicios.js
@@ -3,8 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('servicios', { });
+router.get('/', function (req, res, next) {
+  res.render('servicios', {});
 });
 
 module.exports = router;
